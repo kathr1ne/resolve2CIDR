99fb12f12aa2321618e2030e523e65e2fd125b51
root <root@localhost.localdomain>
2019-05-23 16:21:25 +0800

    update cidr merge

--- IP2CidrTools.py
+++ IP2CidrTools.py
@@ -14,26 +14,36 @@ def dealwithValueError(start, end):
     CIDR = IPSet()
     s = start.split('.')
     e = end.split('.')
-    if s[1] == e[1]:
-	for i in range(int(s[2]), int(e[2])):
-	    s[2] = str(i)
-	    s[3] = '0/24'
-	    CIDR.add(IP('.'.join(s)))
-	s[2] = str(int(s[2]) + 1)
-	s[3] = '0'
-	try:
-	    CIDR.add(IP('{}-{}'.format('.'.join(s), '.'.join(e))))
+    if s[0] == e[0]:
+	if s[1] == e[1]:
+	    for i in range(int(s[2]), int(e[2])):
+		s[2] = str(i)
+		s[3] = '0/24'
+		CIDR.add(IP('.'.join(s)))
+	    s[2] = e[2] #str(int(s[2]) + 1)
+	    s[3] = '0'
+	    try:
+		CIDR.add(IP('{}-{}'.format('.'.join(s), '.'.join(e))))
+		return CIDR
+	    except Exception:
+		CIDR.add(rangeIPd(e))
+		return CIDR
+	elif s[1] != e[1]:
+	    print s, e
+	    for i in range(int(s[1]), int(e[1])):
+		s[1] = e[1] = str(i)
+		CIDR.add(IP('{}-{}'.format('.'.join(s), '.'.join(e))))
+	    print s, e
+	    s[1] = e[1] = str(int(e[1]) + 1)
+	    print s, e
+	    CIDR.add(dealwithValueError('.'.join(s), '.'.join(e)))
 	    return CIDR
-	except Exception:
-	    CIDR.add(rangeIPd(e))
-	    return CIDR
-    elif s[1] != e[1]:
-	for i in range(256):
-	    s[2] = str(i)
-	    s[3] = '0/24'
+    elif s[0] != e[0]:
+	for i in range(int(s[0]), int(e[0])):
+	    s[0] = str(i)
+	    s[3] = '0/8'
 	    CIDR.add(IP('.'.join(s)))
-	s[1] = e[1]
-	s[2] = s[3] = '0'
+	s[0] = e[0]
 	CIDR.add(dealwithValueError('.'.join(s), '.'.join(e)))
 	return CIDR
 
@@ -75,6 +85,8 @@ def convert2CIDR(args, args_type='tolastip'):
 
 if __name__ == '__main__':
     # tolastip = '192.168.0.0-192.168.0.255'
-    tolastip = '2.13.0.0 - 2.14.255.255'
+    # tolastip = '2.13.0.0 - 2.15.255.255'
+    # tolastip = '028.000.000.000-030.255.255.255'
+    tolastip = '017.092.128.000-017.094.255.255'
     cidr = ['192.168.0.0/24', '192.168.1.0/24']
-    print convert2CIDR(cidr, 'cidr')
+    print convert2CIDR(tolastip)
--- handleTxtx.py
+++ handleTxtx.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 
+import time
 import pandas as pd
-from IP2CidrTools import convert2CIDR
-from IPy import IP
-from IPy import IPSet
+from netaddr import iprange_to_cidrs
+from netaddr import cidr_merge
 
 """
 国家两位代码 - GRE
 CN - eth0   # 大陆
-HK - gre1   # Default2HK
+HK - gre1   # default2HK
 SG - gre2   # 新加坡
 US - gre3   # 美国
-TW - gre4   # 台湾
+TW - gre4   # 台湾省
 EU - gre5   # 欧洲地区 对应txtx大洲代码
 JP - gre6   # 日本
 KR - gre7   # 韩国
@@ -24,7 +24,8 @@ AU - gre10  # 澳大利亚
 refer: https://zh.wikipedia.org/wiki/ISO_3166-1
 -----------------------------------------------
 """
-country_codes = ['SG', 'US', 'TW', 'EU', 'JP', 'KR', 'RU', 'VN', 'AU']
+# country_codes = ['SG', 'US', 'TW', 'EU', 'JP', 'KR', 'RU', 'VN', 'AU']
+country_codes = ['SG', 'TW', 'JP', 'KR', 'RU', 'VN', 'AU', 'EU', 'US']
 
 # set colums names
 # refer: https://github.com/ipipdotnet/ipdb-python
@@ -33,9 +34,17 @@ names = ['startIP', 'endIP', 'country_name', 'region_name', 'city_name', 'owner_
 	 'idd_code', 'country_code', 'continent_code']
 
 # read data; set dtype
-data = pd.read_csv('mytestip.txtx', names=names, sep='\s+', dtype='category')
+s1 = time.time()
+# data = pd.read_csv('mytestip.txtx', names=names, sep='\s+', dtype='category')
+data = pd.read_csv('mydata4vipday2.txtx', names=names, sep='\s+', dtype='category')
+e1 = time.time()
+print 'e1s1 spend time {}s'.format(e1 - s1)
+
+s2 = time.time()
 data['startIP'] = data['startIP'].astype('object')
 data['endIP'] = data['endIP'].astype('object')
+e2 = time.time()
+print 'e2s2 spend time {}s'.format(e2 - s2)
 # 纳米比亚 国家两位代码位NA 与pandas默认空值冲突 清洗为NAM
 # data['country_code'] = data['country_code'].cat.add_categories(['NAM']);
 # data.country_code.fillna('NAM', inplace=True)
@@ -45,25 +54,43 @@ data['endIP'] = data['endIP'].astype('object')
 # cols.insert(2, cols.pop())
 # print data[cols]
 
-def generate_df(ccode):
+def filter_ccode(ccode):
     if ccode == 'EU':
 	country_df = data[data['continent_code'] == ccode].copy()
     else:
 	country_df = data[data['country_code'] == ccode].copy()
-    country_df['CIDR'] = country_df['startIP'] + '-' + country_df['endIP']
-    country_df['CIDR'] = country_df['CIDR'].map(convert2CIDR)
+    # country_df['CIDR'] = country_df['startIP'] + '-' + country_df['endIP']
+    # country_df['CIDR'] = country_df['CIDR'].map(function)
+
+    # Capture ValueError
+    # https://stackoverflow.com/questions/55321537/df-apply-valueerror-cannot-set-a-frame-with-no-defined-index-and-a-value-that
+    try:
+	country_df['CIDR'] = country_df.apply(lambda df: iprange_to_cidrs(df['startIP'], df['endIP']), axis=1)
+    except ValueError:
+	pass
     return country_df
 
-def CIDR_IPSet(df):
-    CIDR = IPSet()
-    for i in df.iteritems():
-	CIDR.add(IP(i[1]))
-    return [cidr.strNormal() for cidr in CIDR]
+for ccode in country_codes:
+    l = []
+    s3 = time.time()
+    try:
+	for i in filter_ccode(ccode)['CIDR'].values:
+	    l.extend(i)
+	l = cidr_merge(l)
+	pd.Series(l).to_csv('result/setlist/{}.list'.format(ccode.lower()), index=False, header=False)
+	e3 = time.time()
+	print 'e3s3 {} spend time {}s'.format(ccode, e3 - s3)
+    # Capture KeyError: 'CIDR'
+    except KeyError:
+	pass
 
+
+'''
 for ccode in country_codes:
     try:
-	df = generate_df(ccode)['CIDR'].str.split(',', expand=True).stack()
+	df = filter_ccode(ccode)['CIDR'].str.split(',', expand=True).stack()
 	df = pd.Series(CIDR_IPSet(df))
     except IndexError:
 	df = pd.Series([])
     df.to_csv('result/setlist/{}.list'.format(ccode.lower()), index=False, header=False)
+'''
