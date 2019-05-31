eb9b8419483d3ba1956aa67b6658970aa9087689
krthr1ne <97431110@qq.com>
2019-05-31 11:01:39 +0800

    convert pep8 code style

--- logs.py
+++ logs.py
@@ -4,9 +4,11 @@
 
 import logging
 
+
 def set_log(log_file):
-    logging.basicConfig(level=logging.INFO,
-                format = '%(asctime)s %(filename)s[%(lineno)d]: %(levelname)s %(message)s',
-                datefmt = '%F %T',
-                filename = log_file,
-                filemode = 'w')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(filename)s[%(lineno)d]: %(levelname)s %(message)s',
+        datefmt='%F %T',
+        filename=log_file,
+        filemode='w')
